3dc9fbc4469c2cf47e8795d25491e1cf4f1881a4
jcorks <jcorkery@umich.edu>
2024-07-05 18:16:53 -0400

    Reorganization of extensions, separating out sockets from the rest since theyll often require non-standard libraries

--- src/rom/native.c
+++ src/rom/native.c
@@ -27,6 +27,7 @@ DEALINGS IN THE SOFTWARE.
 
 
 */
+#define _XOPEN_SOURCE 500
 #include "../matte_string.h"
 #include "../matte_vm.h"
 #include "../matte_store.h"
@@ -35,75 +36,90 @@ DEALINGS IN THE SOFTWARE.
 #include "../matte_bytecode_stub.h"
 #include "native.h"
 
+#ifdef MATTE_USE_SYSTEM_EXTENSIONS__ALL
+    #define MATTE_USE_SYSTEM_EXTENSIONS
+    #define MATTE_USE_SYSTEM_EXTENSIONS__BASIC
+    #define MATTE_USE_SYSTEM_EXTENTIONS__SOCKETS
+#endif
 
 #include "./core/json.c"
 #include "./core/memorybuffer.c"
 #ifdef MATTE_USE_SYSTEM_EXTENSIONS
 
 #ifdef __WIN32__
-#define MATTE_IS_WINDOWS
+    #define MATTE_IS_WINDOWS
 
-//////////// put the includes needed for all of them here.
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/time.h>
-#include <errno.h>
-#include <time.h> 
-#include <pthread.h>
-#include <windows.h>
+    //////////// put the includes needed for all of them here.
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
+    #include <unistd.h>
+    #include <sys/time.h>
+    #include <errno.h>
+    #include <time.h> 
+    #include <pthread.h>
+    #include <windows.h>
+    ////////////
 
+    #ifdef MATTE_USE_SYSTEM_EXTENSIONS__BASIC
+        #include "./system/winapi/shared.c"
+        #include "./system/winapi/consoleio.c"
+        #include "./system/winapi/filesystem.c"
+        #include "./system/winapi/time.c"
+        #include "./system/winapi/utility.c"
+        #include "./system/winapi/async.c"
+    #endif
 
-////////////
-#include "./system/winapi/shared.c"
-#include "./system/winapi/consoleio.c"
-#include "./system/winapi/filesystem.c"
-#include "./system/winapi/socketio.c"
-#include "./system/winapi/time.c"
-#include "./system/winapi/utility.c"
-#include "./system/winapi/async.c"
-
+    #ifdef MATTE_USE_SYSTEM_EXTENTIONS__SOCKETS
+        #include "./system/winapi/socketio.c"
+    #endif
 
 
 
 
 #elif defined (__unix__) || (defined (__APPLE__) && defined (__MACH__))
-#define MATTE_IS_POSIX
-//////////// put the includes needed for all of them here.
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/time.h>
-#include <dirent.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <netdb.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <errno.h>
-#include <time.h> 
-#include <pthread.h>
-#include <fcntl.h>
-#include <openssl/bio.h>
-#include <openssl/ssl.h>
-#include <openssl/err.h>
-#include <openssl/pem.h>
-#include <openssl/x509.h>
-#include <openssl/x509_vfy.h>
+    #define MATTE_IS_POSIX
+    //////////// put the includes needed for all of them here.
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
+    #include <unistd.h>
+    #include <sys/time.h>
+    #include <dirent.h>
+    #include <sys/types.h>
+    #include <sys/stat.h>
+    #include <unistd.h>
+    #include <errno.h>
+    #include <time.h> 
+    #include <pthread.h>
+    #include <fcntl.h>
 
 
-////////////
-#include "./system/posix/shared.c"
-#include "./system/posix/consoleio.c"
-#include "./system/posix/filesystem.c"
-#include "./system/posix/socketio.c"
-#include "./system/posix/time.c"
-#include "./system/posix/utility.c"
-#include "./system/posix/async.c"
+
+    ////////////
+    #ifdef MATTE_USE_SYSTEM_EXTENSIONS__BASIC
+        #include "./system/posix/shared.c"
+        #include "./system/posix/consoleio.c"
+        #include "./system/posix/filesystem.c"
+        #include "./system/posix/time.c"
+        #include "./system/posix/utility.c"
+        #include "./system/posix/async.c"
+    #endif        
+        
+    #ifdef MATTE_USE_SYSTEM_EXTENTIONS__SOCKETS
+        #include <netdb.h>
+        #include <sys/socket.h>
+        #include <netinet/in.h>
+        #include <arpa/inet.h>
+        #include <openssl/bio.h>
+        #include <openssl/ssl.h>
+        #include <openssl/err.h>
+        #include <openssl/pem.h>
+        #include <openssl/x509.h>
+        #include <openssl/x509_vfy.h>
+        #include "./system/posix/socketio.c"
+    #endif
+        
 #else
 #error "Unknown system"
 #endif
@@ -113,15 +129,20 @@ DEALINGS IN THE SOFTWARE.
 
 
 void matte_bind_native_functions(matteVM_t * vm) {
-    matte_system__json(vm);
-    matte_system__memorybuffer(vm);
+    #ifdef MATTE_USE_SYSTEM_EXTENSIONS__BASIC
+        matte_system__json(vm);
+        matte_system__memorybuffer(vm);
 
-    matte_system__consoleio(vm);
-    matte_system__filesystem(vm);
-    matte_system__socketio(vm);
-    matte_system__time(vm);
-    matte_system__utility(vm);
-    matte_system__async(vm);
+        matte_system__consoleio(vm);
+        matte_system__filesystem(vm);
+        matte_system__time(vm);
+        matte_system__utility(vm);
+        matte_system__async(vm);
+    #endif
+
+    #ifdef MATTE_USE_SYSTEM_EXTENTIONS__SOCKETS
+        matte_system__socketio(vm);
+    #endif
     
 }
 
